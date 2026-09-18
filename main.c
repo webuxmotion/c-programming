087e2d14e569fcb49395633512bb51b24aa5e654
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2026-09-19 00:59:25 +0300

    recusion

--- main.c
+++ main.c
@@ -1,38 +1,16 @@
 #include <stdio.h>
 
+void process_char() {
+  int c = getchar();
+
+  if (c != EOF) {
+    putchar(c);
+    process_char();
+  }
+};
+
 int main()
 {
-  float fahr, celsius;
-  float lower, upper, step;
-  float lowerCelcius, upperCelcius, stepCelcius; 
-
-  lower = 0.0f; 
-  upper = 300.0f; 
-  step = 20.0f;
-
-  lowerCelcius = -50.0f;
-  upperCelcius = 150.0f;
-  stepCelcius = 10.0f;
-
-  fahr = lower;
-
-  printf("Fahrenheit to Celsius conversion table\n");
-
-  while (fahr <= upper) {
-    celsius = (5.0f / 9.0f) * (fahr - 32.0f);
-    printf("%3.0f\t%6.2f\n", fahr, celsius);
-    fahr = fahr + step;
-  }
-
-  celsius = lowerCelcius;
-
-  printf("\nCelsius to Fahrenheit conversion table\n");
-
-  while (celsius <= upperCelcius) {
-    fahr = (celsius / (5.0f / 9.0f)) + 32.f;
-    printf("%6.0f\t%6.2f\n", celsius, fahr);
-    celsius += stepCelcius;
-  }
-
+  process_char();
   return 0;
 }
